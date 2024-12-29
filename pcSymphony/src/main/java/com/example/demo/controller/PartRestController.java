842f901458aa4de50f70ce8a7dc63b51f560215f
DongwonLim <idongwon01@gmail.com>
2024-12-29 19:16:13 +0900

    20241229 임동원 파츠리뷰 db저장 완

--- pcSymphony/src/main/java/com/example/demo/controller/PartRestController.java
+++ pcSymphony/src/main/java/com/example/demo/controller/PartRestController.java
@@ -1,7 +1,13 @@
 package com.example.demo.controller;
 
+import com.example.demo.domain.dto.part.PartsReviewDTO;
+import com.example.demo.security.MemberUserDetails;
 import com.example.demo.service.PartService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +18,13 @@ public class PartRestController {
 
     private final PartService partService;
 
+    //댓글 저장
+    @PostMapping("partsReviewWrite")
+    public ResponseEntity<?> partsReviewWrite(@RequestBody PartsReviewDTO reviewDTO,
+                                              @AuthenticationPrincipal MemberUserDetails user) {
+        reviewDTO.setMemberId(user.getUsername());
+        partService.partsReviewWrite(reviewDTO);
 
+        return ResponseEntity.ok().build();
+    }
 }
