06471e11cc448d4a8c38c514547b01922faba895
Dongwon_Lim <idongwon01@gmail.com>
2025-01-13 19:20:49 +0900

    20250113 lim 리플날짜성공

--- pcSymphony/src/main/java/com/example/demo/domain/entity/CommunityReplyEntity.java
+++ pcSymphony/src/main/java/com/example/demo/domain/entity/CommunityReplyEntity.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -14,6 +15,7 @@ import java.time.LocalDateTime;
 @Builder
 @Data
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "community_reply")
 public class CommunityReplyEntity {
 
