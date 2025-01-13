e71d1c43278a622fb09eb8df247ba4a5b0d7fb92
DongwonLim <idongwon01@gmail.com>
2025-01-13 10:59:25 +0900

    250113 lim community html 1차 완

--- pcSymphony/src/main/resources/static/js/list.js
+++ pcSymphony/src/main/resources/static/js/list.js
@@ -4,6 +4,18 @@
         location.href = 'write'; // 글쓰기 페이지로 이동
     });
 
+  // 날짜 형식을 yyyy-MM-dd HH:mm 형태로 변환하는 함수
+    function formatDate(dateString) {
+        const date = new Date(dateString);
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0'); // 월은 0부터 시작하므로 +1
+        const day = String(date.getDate()).padStart(2, '0');
+        const hours = String(date.getHours()).padStart(2, '0');
+        const minutes = String(date.getMinutes()).padStart(2, '0');
+
+        return `${year}-${month}-${day} ${hours}:${minutes}`;
+    }
+
     // 목록 조회 함수
     function getList() {
     $.ajax({
@@ -18,13 +30,14 @@
 
     // 목록을 테이블에 추가
     $(list).each(function(idx, board) {
+    const formattedDate = formatDate(board.communityDate);
     let html = `
                                 <tr>
                                     <td>${board.communityId}</td>
                                     <td><a href="read?communityId=${board.communityId}">${board.communityTitle}</a></td>
                                     <td>${board.memberId}</td>
                                     <td>${board.communityView}</td>
-                                    <td>${board.communityDate}</td>
+                                    <td>${formattedDate}</td>
                                 </tr>
                             `;
     $('#tbody').append(html); // 테이블에 추가
